6f86397d1f4cedac41fd742d01c51db0123f5d41
XianLabs <44497570+XianLabs@users.noreply.github.com>
2020-04-11 18:37:59 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,23 +11,50 @@ using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.Contracts;
 using Nethereum.Web3.Accounts.Managed;
 using NethTest;
+using static NethTest.DBManager;
+using Nethereum.JsonRpc.Client;
 
 namespace Nethereum.CQS.SimpleTokenTransfer
 {
     public class Program
     {
         static void Main(string[] args) //use args/cmd line to get recv address + time + amount, add to db
-        {        
+        {
+
             string FromAddr = args[0];
             string ToAddr = args[1];
-            int Amount = Convert.ToInt32(args[2]);
+            uint Amount = 0;
 
-            ERC20Sender Manager = new ERC20Sender(FromAddr, "key",  ToAddr, Amount);
+            try
+            {
+                Amount = Convert.ToUInt32(args[2]);
+            }
+            catch
+            {
+                Console.WriteLine("invalid command line argument: Amount [2]");
+                return;
+            }
 
+            DBManager DB = new DBManager();
+      
+            ScramblerEntry SE = DB.GetOldestEntry();
+
+            if(SE.FromAddr == String.Empty)
+            {
+                Console.WriteLine("Couldn't fetch DB entry!");
+            }
+
+            Console.WriteLine(SE.FromAddr + " " + SE.ToAddr + " " + SE.Amount + " " + SE.Gas);
+
+
+            ERC20Sender Manager = new ERC20Sender(FromAddr, "GoAwayFromMyKeySour", ToAddr, Amount);
+
+            //todo: get complete transaction before sending off to next wallet
             Console.WriteLine("Checking the balance");
             Manager.BalanceAsync().Wait();
-            Console.WriteLine("Transfering...");
-            Manager.TransferAsync().Wait();
+
+            //Console.WriteLine("Transfering...");
+            Manager.TransferAsync(ToAddr, Amount).Wait();
             Console.ReadLine();
         }
     }
