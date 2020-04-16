c85b5011244cdd6997a17f0ed3ca66efb4f37311
XianLabs <44497570+XianLabs@users.noreply.github.com>
2020-04-15 20:04:13 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,4 @@
 using System;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using Nethereum.ABI.FunctionEncoding.Attributes;
-using Nethereum.Contracts.CQS;
-using Nethereum.Util;
-using Nethereum.Web3.Accounts;
-using Nethereum.Hex.HexConvertors.Extensions;
-using Nethereum.Contracts;
-using Nethereum.Web3.Accounts.Managed;
-using NethTest;
-using static NethTest.DBManager;
-using Nethereum.JsonRpc.Client;
 using System.Threading;
 
 namespace NethTest
@@ -21,7 +7,6 @@ namespace NethTest
     {
         static void Main(string[] args) //use args/cmd line to get recv address + time + amount, add to db
         {
-
             string FromAddr = args[0];
             string ToAddr = args[1];
             uint Amount = 0;
@@ -30,6 +15,7 @@ namespace NethTest
             try
             {
                 Amount = Convert.ToUInt32(args[2]);
+                Gas = Convert.ToUInt32(args[3]);
             }
             catch
             {
@@ -37,17 +23,7 @@ namespace NethTest
                 return;
             }
       
-            try
-            {
-                Gas = Convert.ToUInt32(args[3]);
-            }
-            catch
-            {
-                Console.WriteLine("invalid command line argument: Gas [3]");
-                return;         
-            }
-            
-            ERC20Sender Manager = new ERC20Sender(FromAddr, args[4], ToAddr, Amount, Gas);
+            ERC20Sender Manager = new ERC20Sender(FromAddr, "<privkey>", ToAddr, Amount, Gas);
 
             Thread QueryThread = new Thread(Manager.QueryForSubmissions);
             QueryThread.Start();
